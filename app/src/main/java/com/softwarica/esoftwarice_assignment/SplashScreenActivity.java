a61504df87bc7e2d3d20bc00a36ce4cfe00f1cb9
maharjansumina <suminamaharjan098@gmail.com>
2019-12-27 02:46:15 +0545

    SplashScreen Activity with code Added.

---
+++ app/src/main/java/com/softwarica/esoftwarice_assignment/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.softwarica.esoftwarice_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // Using a thread and halt screen for 2 seconds
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
